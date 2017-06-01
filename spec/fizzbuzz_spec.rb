fd09a09115196709a6178d60681125ee257d7dc7
Simon Hempenstall <simonjhemp@hotmail.com>
2017-06-01 12:28:54 +0100

    Make tests work

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,57 @@
+require "fizzbuzz"
+
+describe "fizzbuzz" do
+  it "returns 'fizz' when passed 3" do
+    expect(fizzbuzz(3)).to eq "fizz"
+  end
+
+  describe "fizzbuzz" do
+    it "returns 'buzz' when passed 5" do
+      expect(fizzbuzz(5)).to eq "buzz"
+    end
+  end
+
+
+  describe "fizzbuzz" do
+    it "returns 'fizzbuzz' when passed 15" do
+      expect(fizzbuzz(15)).to eq "fizzbuzz"
+    end
+  end
+
+  describe "fizzbuzz" do
+    it "returns 1 when passed 1" do
+      expect(fizzbuzz(1)).to eq 1
+    end
+  end
+
+  describe "fizzbuzz" do
+    it "returns 7 when passed 7" do
+      expect(fizzbuzz(7)).to eq 7
+    end
+  end
+
+  describe "fizzbuzz" do
+    it "returns 'buzz' when passed 10" do
+      expect(fizzbuzz(10)).to eq "buzz"
+    end
+  end
+
+  describe "fizzbuzz" do
+    it "returns 'fizz' when passed 9" do
+      expect(fizzbuzz(9)).to eq "fizz"
+    end
+  end
+
+  describe "fizzbuzz" do
+    it "returns 'fizzbuzz' when passed 30" do
+      expect(fizzbuzz(30)).to eq "fizzbuzz"
+  end
+  end
+
+  describe "fizzbuzz" do
+    it "returns 'fizzbuzz' when passed 75" do
+      expect(fizzbuzz(75)).to eq "fizzbuzz"
+  end
+end
+
+end
